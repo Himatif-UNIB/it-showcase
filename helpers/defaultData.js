c4cfdda3604e8baa16079dbf6aea7ddf758d87ec
WahyuS002 <syahputrawahyu61@gmail.com>
2022-06-15 09:57:25 +0700

    :white_check_mark: added default helpers

---
+++ helpers/defaultData.js
@@ -0,0 +1,9 @@
+export const defaultTypes = [
+    { identifier: "app", label: "App" },
+    { identifier: "multimedia", label: "Multimedia" },
+]
+
+export const defaultCategories = [
+    { identifier: "sistem-informasi", label: "Sistem Informasi" },
+    { identifier: "apps", label: "Apps" },
+]
